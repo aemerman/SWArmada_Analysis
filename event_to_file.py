41c7926f522dd336ed945722469995771b78644e
Balthyde <alex.emerman@gmail.com>
2025-04-10 11:39:41 -0700

    bugfix: fleet data now stored in correct columns

--- event_to_file.py
+++ event_to_file.py
@@ -257,7 +257,7 @@ def get_fleet_lists(fleets, conn, ev_id):
         INSERT INTO Fleets_Ships (fleet_id, ship_id) VALUES (?, ?)
         """
         insert_upgrades_str = """
-        INSERT INTO Fleets_Upgrades (fleet_id, upgrade_id, ship_id)
+        INSERT INTO Fleets_Upgrades (fleet_id, upgrade_id, fleet_ship_id)
         VALUES (?, ?, ?)
         """
         insert_squadrons_str = """
@@ -286,12 +286,12 @@ def get_fleet_lists(fleets, conn, ev_id):
                 continue
 
             for upgrade in ship['upgrades']:
-                upgrade_values = (upgrade['id'], fleet_id, fleet_ship_id,)
+                upgrade_values = (fleet_id, upgrade['id'], fleet_ship_id)
                 cursor.execute(insert_upgrades_str, upgrade_values)
 
         for squad in fleet['squadrons']:
             count = squad.get('count', 1)
-            squad_values = (squad['id'], fleet_id, count,)
+            squad_values = (fleet_id, squad['id'], count)
             cursor.execute(insert_squadrons_str, squad_values)
         conn.commit()
 
